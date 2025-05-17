6abfd30c8432921173a729bc98c2d3896550ce16
charanbuchannola <buchannolacharan@gmail.com>
2025-05-17 18:59:02 +0530

    Add MongoDB connection setup in db.js

---
+++ server/src/db/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connect = () => {
+  mongoose
+    .connect(
+      "mongodb+srv://buchannolacharan:ghpsNKrS7eVnXUV8@cluster0.olj9y.mongodb.net/notice_analyzer"
+    )
+    .then(() => console.log("MongoDB Connected..."))
+    .catch((err) => console.error("Could not connect to MongoDB:", err));
+};
+
+module.exports = connect;
