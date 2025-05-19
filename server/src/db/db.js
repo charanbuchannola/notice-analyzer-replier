b227822dc574d073327656f59824bd56dbb69de5
charanbuchannola <buchannolacharan@gmail.com>
2025-05-19 06:07:11 +0530

    fix: update MongoDB connection in db.js to use environment variable for URI

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
+  const uri = process.env.MONGODB_URI;
+  if (!uri) {
+    console.error("MONGODB_URI not set in environment variables!");
+    process.exit(1);
+  }
   mongoose
-    .connect(
-      "mongodb+srv://buchannolacharan:WMCRmAknvFFY6F8Q@cluster0.olj9y.mongodb.net/notice_analyzer"
-    )
+    .connect(uri)
     .then(() => console.log("MongoDB Connected..."))
     .catch((err) => console.error("Could not connect to MongoDB:", err));
 };
