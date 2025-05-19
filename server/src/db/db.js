db9e46fc671bf2d915866e26360bed8db734ef84
charanbuchannola <buchannolacharan@gmail.com>
2025-05-19 06:16:55 +0530

    fix: restore hardcoded MongoDB connection URI in db.js

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,14 +1,10 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-  const uri =
-    "mongodb+srv://buchannolacharan:WMCRmAknvFFY6F8Q@cluster0.olj9y.mongodb.net/notice_analyzer";
-  if (!uri) {
-    console.error("MONGODB_URI not set in environment variables!");
-    process.exit(1);
-  }
   mongoose
-    .connect(uri)
+    .connect(
+      "mongodb+srv://buchannolacharan:WMCRmAknvFFY6F8Q@cluster0.olj9y.mongodb.net/notice_analyzer"
+    )
     .then(() => console.log("MongoDB Connected..."))
     .catch((err) => console.error("Could not connect to MongoDB:", err));
 };
